cac042dd6311260f65b7ac83c73bc5ed6427d35e
Matt Kane <matt@gatsbyjs.com>
2020-03-09 17:01:22 +0000

    Add built files

---
+++ gatsby-node.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const fetchData = require('./fetch');
+
+const {
+  createNodeHelper
+} = require('./utils');
+
+const {
+  createGatsbyImageResolver
+} = require('./gatsby-image-resolver');
+
+exports.sourceNodes = async ({
+  actions,
+  webhookBody,
+  createContentDigest,
+  getNode
+}, {
+  apiURL = 'https://api.cosmicjs.com/v1',
+  bucketSlug = '',
+  objectTypes = [],
+  apiAccess = {},
+  preview = false,
+  localMedia = false
+}) => {
+  const {
+    createNode,
+    deleteNode
+  } = actions;
+  const helperObject = {
+    createContentDigest,
+    createNode,
+    localMedia
+  };
+  console.log(webhookBody);
+  /*
+   * Gatsby preview code path!
+   */
+
+  if (webhookBody && webhookBody.type) {
+    const item = webhookBody.data;
+
+    switch (webhookBody.type) {
+      case 'object.deleted':
+        if (Array.isArray(item) && item.length) {
+          for (const id of item) {
+            deleteNode({
+              node: getNode(id)
+            });
+          }
+        }
+
+        break;
+
+      case 'object.created.draft':
+      case 'object.created.published':
+      case 'object.edited.draft':
+      case 'object.edited.published':
+        createNodeHelper(item, helperObject);
+        break;
+
+      default:
+        break;
+    }
+
+    return;
+  }
+  /*
+   * The existing, non-preview code path!
+   */
+
+
+  const promises = objectTypes.map(objectType => fetchData({
+    apiURL,
+    bucketSlug,
+    objectType,
+    apiAccess,
+    preview
+  })); // Execute the promises.
+
+  const data = await Promise.all(promises); // Create nodes.
+
+  objectTypes.forEach((_item, i) => {
+    var items = data[i];
+    items.forEach(item => {
+      createNodeHelper(item, helperObject);
+    });
+  });
+};
+
+exports.createResolvers = createGatsbyImageResolver;
